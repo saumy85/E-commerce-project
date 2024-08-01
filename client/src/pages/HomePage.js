32810a11da280dd8671b5787d878ab05305cafc1
saumy85 <saumypandey7080@gmail.com>
2024-08-01 21:02:13 +0530

    update

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -23,7 +23,9 @@ const HomePage = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("/api/v1/category/get-category");
+      const { data } = await axios.get(
+        "https://e-commerce-project-m5qv.onrender.com/api/v1/category/get-category"
+      );
       if (data?.success) {
         setCategories(data?.category);
       }
